3b5f86ba54148f8ed05f79b2beafca6ddd6023b1
angelo <a20192258@pucp.edu.pe>
2023-05-07 01:30:42 -0500

    More 3

--- app/src/main/java/com/example/lab3_20192258/Begin.java
+++ app/src/main/java/com/example/lab3_20192258/Begin.java
@@ -2,15 +2,22 @@ package com.example.lab3_20192258;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.lab3_20192258.databinding.FragmentBeginBinding;
+
+import java.util.ArrayList;
 
 
 public class Begin extends Fragment {
