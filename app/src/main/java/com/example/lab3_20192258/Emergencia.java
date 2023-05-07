bd672d0b76ec570d928aaa59ffc126fc404e4cc1
angelo <a20192258@pucp.edu.pe>
2023-05-07 10:30:45 -0500

    Mapa

--- app/src/main/java/com/example/lab3_20192258/Emergencia.java
+++ app/src/main/java/com/example/lab3_20192258/Emergencia.java
@@ -3,13 +3,14 @@ package com.example.lab3_20192258;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.lab3_20192258.databinding.FragmentEmergenciaBinding;
-import com.example.lab3_20192258.databinding.FragmentRegistroBinding;
 
 
 public class Emergencia extends Fragment {
@@ -19,6 +20,19 @@ public class Emergencia extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentEmergenciaBinding.inflate(inflater,container,false);
+
+        // Get the FragmentManager for the child fragments
+        FragmentManager childFragmentManager = getChildFragmentManager();
+
+        // Begin a FragmentTransaction
+        FragmentTransaction transaction = childFragmentManager.beginTransaction();
+
+        // Create the nested fragment and add it to the container fragment
+        MapFragment nestedFragment = new MapFragment();
+        transaction.add(R.id.frame_layout, nestedFragment);
+
+        // Commit the FragmentTransaction
+        transaction.commit();
 
         return binding.getRoot();
     }
