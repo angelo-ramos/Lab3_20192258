aa5d03461e0d673b68154c426c19ac11b39eea30
angelo <a20192258@pucp.edu.pe>
2023-05-07 01:13:44 -0500

    More 2

--- app/src/main/java/com/example/lab3_20192258/Begin.java
+++ app/src/main/java/com/example/lab3_20192258/Begin.java
@@ -1,30 +1,29 @@
 package com.example.lab3_20192258;
 
-import android.content.Context;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.lab3_20192258.databinding.FragmentBeginBinding;
-import com.example.lab3_20192258.databinding.FragmentRegistroBinding;
 
 
-public class Registro extends Fragment {
-    FragmentRegistroBinding binding;
+public class Begin extends Fragment {
+    FragmentBeginBinding binding;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        binding = FragmentRegistroBinding.inflate(inflater,container,false);
+        binding = FragmentBeginBinding.inflate(inflater,container,false);
+
+        NavController navController = NavHostFragment.findNavController(Begin.this);
+        binding.Registro.setOnClickListener( view -> {
+            navController.navigate(R.id.action_begin_to_registro);
+        });
 
         return binding.getRoot();
     }
--- app/src/main/java/com/example/lab3_20192258/MainActivity.java
+++ app/src/main/java/com/example/lab3_20192258/MainActivity.java
@@ -1,15 +1,37 @@
 package com.example.lab3_20192258;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.lab3_20192258.databinding.ActivityMainBinding;
 
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+    private NavController navController; //Para el botón atrás del toolbar
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //Las 3 líneas del binding para un Activity (Para fragment es ligeramente distinto ps son 2)
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        getSupportActionBar().setTitle("Ambulancias Mascotín");
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Para poner boton atras en cada fragment, poner estas 2 líneas (el nav_host .. esta en activity_main.xml y es un fragment!)
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        NavigationUI.setupActionBarWithNavController(this, navController);
+    }
+
+
+
+    //Método para la navegación hacia atrás (Sirve para el botón hacia atrás)
+    @Override
+    public boolean onSupportNavigateUp() {
+        // Handle the back button event
+        return NavigationUI.navigateUp(navController, (DrawerLayout) null);
     }
 }
